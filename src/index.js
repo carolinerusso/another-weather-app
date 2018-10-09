3f3e2c86d3917a05bf0964e038c344c1ccc6bccb
Caroline Russo <>
2018-10-09 12:21:07 -0400

    Add endpoint to props

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import Styles from './main.scss';
 import Weather from './js/Weather.jsx';
 
 ReactDOM.render(
-	<Weather defaultZip="06830" />, 
+	<Weather defaultZip="06830" endpoint="https://1ljygy7hv5.execute-api.us-east-2.amazonaws.com/production/" />, 
 	document.querySelector('.weather')
 );
